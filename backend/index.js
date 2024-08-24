f776ae78145a938260d08fde1aaa60296de09a90
KhushiBhardwaj11 <bhardwajkhushi11203@gmail.com>
2024-08-23 20:22:13 -0700

    seventh commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import databaseConnection from './config/database.js';
 import cookieParser from "cookie-parser";
 import userRoute from './routes/userRoute.js'
+import tweetRoute from './routes/tweetRoute.js'
 
 dotenv.config({
     path:".env"
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 //api
 app.use("/api/v1/user",userRoute);
+app.use("/api/v1/tweet",tweetRoute);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server listen at Port ${process.env.PORT}`);
