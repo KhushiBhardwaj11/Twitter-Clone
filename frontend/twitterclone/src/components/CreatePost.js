b8f89dbb3a01f1d74a20272587900f9b4a88ff7f
KhushiBhardwaj11 <bhardwajkhushi11203@gmail.com>
2024-08-29 22:21:12 -0700

    ninth commit

--- frontend/twitterclone/src/components/CreatePost.js
+++ frontend/twitterclone/src/components/CreatePost.js
@@ -1,16 +1,69 @@
-import React from 'react'
-import Avatar from "react-avatar"
+import React from "react";
+import Avatar from "react-avatar";
 import { CiImageOn } from "react-icons/ci";
+import axios from "axios";
+import { TWEET_API_END_POINT } from "../utils/constant.js";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTweets, getIsActive, getRefresh } from "../redux/tweetSlice.js";
+import { useState } from "react";
 
 const CreatePost = () => {
+  const [description, setDescription] = useState("");
+  const { user } = useSelector(store => store.user);
+  const {isActive}=useSelector(store=>store.tweet);
+  const dispatch = useDispatch();
+
+  const submitHandler = async () => {
+    try {
+      const res = await axios.post(
+        `${TWEET_API_END_POINT}/create`,
+        { description, id: user?._id },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(getRefresh());
+      if (res.data.success) {
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error);
+    }
+    setDescription("");
+  };
+ 
+  const forYouHandler=()=>{
+     dispatch(getIsActive(true));
+  }
+
+  const followingHandler = () => {
+    dispatch(getIsActive(false));
+  };
+
   return (
     <div className="w-[100%]">
       <div>
         <div className="flex items-center justify-evenly border-b border-gray-200">
-          <div className="cursor-pointer hover:bg-gray-200 w-full text-center px-4 py-3">
+          <div
+            onClick={forYouHandler}
+            className={`${
+              isActive
+                ? "border-b-4 border-blue-600 "
+                : "border-b-4 border-transparent"
+            } cursor-pointer hover:bg-gray-200 w-full text-center px-4 py-3`}
+          >
             <h1 className="font-semibold text-gray-600 text-lg ">For you</h1>
           </div>
-          <div className="cursor-pointer  hover:bg-gray-200 w-full text-center px-4 py-3">
+          <div
+            onClick={followingHandler}
+            className={`${
+              !isActive
+                ? "border-b-4 border-blue-600 "
+                : "border-b-4 border-transparent"
+            } cursor-pointer  hover:bg-gray-200 w-full text-center px-4 py-3`}
+          >
             <h1 className="font-semibold text-gray-600 text-lg ">Following</h1>
           </div>
         </div>
@@ -25,6 +78,8 @@ const CreatePost = () => {
               />
             </div>
             <input
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
               className="w-full outline-none border-none text-xl ml-2 "
               type="text"
               placeholder="What is happening?!"
@@ -34,7 +89,10 @@ const CreatePost = () => {
             <div>
               <CiImageOn size="24px" />
             </div>
-            <button className="bg-[#1D9BF0]  px-4 py-1 text-lg text-white text-right border-none rounded-full ">
+            <button
+              onClick={submitHandler}
+              className="bg-[#1D9BF0]  px-4 py-1 text-lg text-white text-right border-none rounded-full "
+            >
               Post
             </button>
           </div>
@@ -42,6 +100,6 @@ const CreatePost = () => {
       </div>
     </div>
   );
-}
+};
 
-export default CreatePost
+export default CreatePost;
--- frontend/twitterclone/src/components/Tweet.js
+++ frontend/twitterclone/src/components/Tweet.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React from "react";
 import Avatar from "react-avatar";
 import { CiHeart } from "react-icons/ci";
 import { FaRegComment } from "react-icons/fa";
 import { CiBookmark } from "react-icons/ci";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import axios from "axios";
+import { TWEET_API_END_POINT } from "../utils/constant.js";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { getRefresh } from "../redux/tweetSlice.js";
 
-const Tweet = () => {
+const Tweet = ({ tweet }) => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const likeOrDislikeHandler = async (id) => {
+    try {
+      const res = await axios.put(
+        `${TWEET_API_END_POINT}/like/${id}`,
+        { id: user?._id },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(getRefresh());
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.success(error.response.data.message);
+      console.log(error);
+    }
+  };
+
+  const deleteTweetHandler=async (id)=>{
+    try {
+      axios.defaults.withCredentials=true;
+      const res = await axios.delete(`${TWEET_API_END_POINT}/delete/${id}`);
+      console.log(res);
+      dispatch(getRefresh());
+      toast.success(res.data.message);
+      } catch (error) {
+      toast.success(error.response.data.message);
+      console.log(error);
+    }
+  }
   return (
-    <div className='border-b border-gray-200'>
+    <div className="border-b border-gray-200">
       <div>
         <div className="flex p-4">
           <Avatar
@@ -16,12 +53,14 @@ const Tweet = () => {
           />
           <div className="ml-2 w-full">
             <div className="flex items-center ">
-              <h1 className="font-bold">Patel</h1>
-              <p className="text-gray-500 text-sm ml-1">@patelmernstack . 1m</p>
+              <h1 className="font-bold">{tweet?.userDetails[0]?.name}</h1>
+              <p className="text-gray-500 text-sm ml-1">
+                {`@${tweet?.userDetails[0]?.username} . 1m`}
+              </p>
             </div>
 
             <div>
-              <p>Hello Developers lets connect and grow together.</p>
+              <p>{tweet?.description}</p>
             </div>
 
             <div className="flex justify-between my-3">
@@ -32,10 +71,13 @@ const Tweet = () => {
                 <p>0</p>
               </div>
               <div className="flex items-center">
-                <div className="p-2 hover:bg-pink-200 rounded-full cursor-pointer">
+                <div
+                  onClick={() => likeOrDislikeHandler(tweet?._id)}
+                  className="p-2 hover:bg-pink-200 rounded-full cursor-pointer"
+                >
                   <CiHeart size="24px" />
                 </div>
-                <p>0</p>
+                <p>{tweet?.like?.length}</p>
               </div>
               <div className="flex items-center">
                 <div className="p-2 hover:bg-yellow-200 rounded-full cursor-pointer">
@@ -43,12 +85,19 @@ const Tweet = () => {
                 </div>
                 <p>0</p>
               </div>
+              {user?._id === tweet?.userId && (
+                <div onClick={()=>deleteTweetHandler(tweet?._id)} className="flex items-center">
+                  <div className="p-2 hover:bg-red-300 rounded-full cursor-pointer">
+                    <MdOutlineDeleteOutline size="24px" />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Tweet
+export default Tweet;
